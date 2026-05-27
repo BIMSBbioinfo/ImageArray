377b546d6ba43421a8a7c790bffeaf6e78e3068d
Artur-man <artur-man@hotmail.com>
2026-05-27 14:17:35 +0200

    initial imp. for delayed transformations

--- R/allgenerics.R
+++ R/allgenerics.R
@@ -9,3 +9,8 @@ setGeneric("axes", function(object, ...) standardGeneric("axes"))
 setGeneric("rotate")
 setGeneric("flip")
 setGeneric("flop")
+
+# transformations
+setGeneric("scale_transform", \(x, ...) standardGeneric("scale_transform"))
+setGeneric("affine_transform", \(x, ...) standardGeneric("affine_transform"))
+setGeneric("translate_transform", \(x, ...) standardGeneric("translate_transform"))
