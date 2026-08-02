0c77c5e3141b11133ac60db68699d76817fbbba2
Artur-man <artur-man@hotmail.com>
2026-08-02 20:55:00 +0200

    add script back

---
+++ R/AllGenerics.R
@@ -0,0 +1,21 @@
+#' @importMethodsFrom BiocGenerics scale
+#' @importFrom EBImage rotate flip flop
+NULL
+
+# generics of ImageArray
+setGeneric("crop", function(object, ...) standardGeneric("crop"))
+setGeneric("negate", function(object, ...) standardGeneric("negate"))
+setGeneric("modulate", function(object, ...) standardGeneric("modulate"))
+setGeneric("scales", function(object, ...) standardGeneric("scales"))
+setGeneric("axes", function(object, ...) standardGeneric("axes"))
+setGeneric("extent", \(x, ...) standardGeneric("extent"))
+setGeneric(".extent", \(x, ...) standardGeneric(".extent"))
+
+# generics from EBImage
+setGeneric("rotate")
+setGeneric("flip")
+setGeneric("flop")
+
+# transformations
+setGeneric("affine", \(x, ...) standardGeneric("affine"))
+setGeneric("translation", \(x, ...) standardGeneric("translation"))
